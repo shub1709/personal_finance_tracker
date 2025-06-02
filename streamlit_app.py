07cfa4baf398399939c95b1cc67a45e5420492f8
shub1709 <shubhamnits@gmail.com>
2025-06-02 16:55:47 +0530

    Update streamlit_app.py
    trying to hide "manage app" button

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,9 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
+            button[kind="icon"] {
+                        display: none !important;
+        }
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
